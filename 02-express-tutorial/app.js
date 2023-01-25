4b0b9341735a450078fb47087a5ed8b9d9d71762
imonraj27 <77281501+imonraj27@users.noreply.github.com>
2023-01-25 19:05:36 +0530

    gfgf

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,2 +1,25 @@
-console.log('Express Tutorial')
-// i am here
+const express = require('express') // it is a function 
+// it returns a function named express
+
+const path = require('path')
+
+const app = express()
+
+
+//setup static and middleware
+app.use(express.static("./public"))
+
+app.get('/', (req, res) => {
+    console.log('user hits the homepage');
+    res.status(200).sendFile(path.resolve(__dirname, "./navbar-app/index.html"))
+})
+
+
+
+app.all('*', (req, res) => {
+    res.status(404).send('CONTENT NOT FOUND')
+})
+
+app.listen(5000, () => {
+    console.log("SERVER IS LISTENING");
+})
